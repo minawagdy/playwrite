ee1ee3a0a2dc4082344e9ce92e1e1e3886c75b23
minawagdy <minaw77@gmail.com>
2026-05-20 15:23:05 +0300

    test: update todo.spec.ts and user.spec.ts

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -1,32 +1,32 @@
-// import { test, expect } from "@playwright/test";
-// import User from "../models/User";
-// import TodoPage from "../pages/TodoPage";
-// import RegisterPage from "../pages/RegisterPage";
-// import NewTodoPage from "../pages/NewTodoPage";
-// test("should be able to add an item to the todo list", async ({ page, request, context }) => {
-//     const user = new User();
-//     const registerPage = new RegisterPage(page, request, context);
-//     await registerPage.registerUsingTheAPI(user);
-//     const newtodopage = new NewTodoPage(page);
-//     await newtodopage.load();
-//     await newtodopage.addNewTask('playwrite');
-//     const todoPage = new TodoPage(page);
-//     const todoItem = await todoPage.getTodoTextByIndex(0);
-//     expect(todoItem).toEqual('playwrite');
-// });
+import { test, expect } from "@playwright/test";
+import User from "../models/User";
+import TodoPage from "../pages/TodoPage";
+import RegisterPage from "../pages/RegisterPage";
+import NewTodoPage from "../pages/NewTodoPage";
+test("should be able to add an item to the todo list", async ({ page, request, context }) => {
+    const user = new User();
+    const registerPage = new RegisterPage(page, request, context);
+    await registerPage.registerUsingTheAPI(user);
+    const newtodopage = new NewTodoPage(page);
+    await newtodopage.load();
+    await newtodopage.addNewTask('playwrite');
+    const todoPage = new TodoPage(page);
+    const todoItem = await todoPage.getTodoTextByIndex(0);
+    expect(todoItem).toEqual('playwrite');
+});
 
-// test("should be able to delete an item from the todo list", async ({ page, request, context }) => {
-//     const user = new User();
-//     const registerPage = new RegisterPage(page, request, context);
-//     await registerPage.registerUsingTheAPI(user);
+test("should be able to delete an item from the todo list", async ({ page, request, context }) => {
+    const user = new User();
+    const registerPage = new RegisterPage(page, request, context);
+    await registerPage.registerUsingTheAPI(user);
 
-//     const newTodoPage = new NewTodoPage(page, request)
-//     await newTodoPage.addNewTaskUsingAPI(user);
+    const newTodoPage = new NewTodoPage(page, request)
+    await newTodoPage.addNewTaskUsingAPI(user);
 
-//     const todoPage = new TodoPage(page);
-//     await todoPage.load();
-//     await todoPage.deleteTodoByIndex(0);
+    const todoPage = new TodoPage(page);
+    await todoPage.load();
+    await todoPage.deleteTodoByIndex(0);
 
-//     const noTodoMessage = todoPage.getNoTodosMessage();
-//     await expect(noTodoMessage).toBeVisible();
-// });
+    const noTodoMessage = todoPage.getNoTodosMessage();
+    await expect(noTodoMessage).toBeVisible();
+});
